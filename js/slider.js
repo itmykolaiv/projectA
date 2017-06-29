e92b3ee70e01bfcedc45facdd95f7dc258734b6b
AllaSmyshlyaeva <alla.smyshlyaeva@gmail.com>
2017-06-30 01:39:01 +0300

    update structure

--- js/slider.js
+++ js/slider.js
@@ -4,10 +4,10 @@ setInterval(function(){
 	
   //console.log(active_slide);
 	
+  active_slide++;
   if(active_slide > slide_count ) {
   	active_slide = 1;
   }
   document.querySelector('#slider .container').style.marginLeft =  -1* (active_slide-1) * 100 + "%";
-  ++active_slide;
   console.log(document.querySelector('#slider .container').style.marginLeft);
-}, 3000);
+}, 8000);
